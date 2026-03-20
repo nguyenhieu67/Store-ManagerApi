e91d678d738172e2988a8ee61ee29453d850d008
nguyenhieu67 <nguyenduchieu05082000@gmail.com>
2026-03-20 18:29:20 +0900

    fix client typo

--- models/Client.js
+++ models/Client.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const clientSchema = new mongoose.Schema({
-    fisrtName: { type: String, required: true },
+    firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, unique: true },
     phone: String,
